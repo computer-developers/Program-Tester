251a5dedd87dde1c736cee2f547535199a7d61ef
unknown <neel.patel.2012.np@gmail.com>
2017-01-26 11:31:57 +0530

    add dk.briks.automaton package & update lib.regularExpression. dk.briks.automaton package is used in generating regular expression.

---
+++ src/lib/regularExpression/util/Iterable.java
@@ -0,0 +1,9 @@
+package lib.regularExpression.util;
+
+/**
+ * @author neel patel
+ *
+ */
+public interface Iterable {
+	Iterator iterator();
+}
---
+++ src/lib/regularExpression/util/Iterator.java
@@ -0,0 +1,10 @@
+package lib.regularExpression.util;
+
+/**
+ * @author neel patel
+ *
+ */
+public interface Iterator {
+	boolean hasNext();
+	String next();
+}
