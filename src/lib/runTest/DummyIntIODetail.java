2e943bb3ca195e1616db6d88431f8dc892abad02
Neel Patel <neel.patel.2012.np@gmail.com>
2017-02-02 16:44:52 +0530

    no message

---
+++ src/lib/runTest/DummyIntIODetail.java
@@ -0,0 +1,40 @@
+package lib.runTest;
+import java.util.List;
+import lib.runDetails.*;
+import java.util.*;
+/**
+ *
+ * @author Neel Patel
+ */
+public class DummyIntIODetail implements IntIODetail{
+
+     long time;
+     List<String> input,output;
+     private DummyIntIODetail(){}
+     static public IntIODetail getIntIODetail(List<String> input,List<String> output,long time){
+          DummyIntIODetail d=new DummyIntIODetail();
+          d.input=input;
+          d.output=output;
+          d.time=time;
+          return d;
+     }
+     
+     public List<String> getAllOutput() {
+          return output;
+     }
+
+     public String getName() {
+          throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     }
+
+     @Override
+     public long getTime() {
+          return time;
+     }
+
+     @Override
+     public List<String> getAllInput() {
+          return input;
+     }
+     
+}
