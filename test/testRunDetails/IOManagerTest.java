5c4cd6f3d74c000e3efe430eb857ba3e63edddf0
Rushabh Modi <modi.rushabh179@gmail.com>
2017-02-04 20:01:21 +0530

    Encrypted

--- test/testRunDetails/IOManagerTest.java
+++ test/testRunDetails/IOManagerTest.java
@@ -4,11 +4,34 @@
  * and open the template in the editor.
  */
 package testRunDetails;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.*;
+import java.util.List;
+import lib.runDetails.*;
 
 /**
  *
- * @author darsh_000
+ * @author Rushabh Modi
  */
 public class IOManagerTest {
-    
+    public static void main(String... args) throws IOException{
+        IntIODetail io;
+        Path pi;
+        if(args.length>0)
+               pi=Paths.get(args[0]);
+          else
+               pi=Paths.get("input.txt");
+          System.out.println("pi :- "+pi);
+          List<String> input=Files.readAllLines(pi);
+        if(args.length>1)
+               pi=Paths.get(args[1]);
+          else
+               pi=Paths.get("output.txt");
+          System.out.println("pi :- "+pi);
+          List<String> output=Files.readAllLines(pi);
+        io=IOManager.getIODetail(input, output);
+        pi=Paths.get("Data").toAbsolutePath();
+        System.out.println(IOManager.writeIntIODetail(io, pi));
+    }
 }
---
+++ test/testRunDetails/IOManagerTest2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testRunDetails;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.*;
+import java.util.List;
+import java.util.Scanner;
+import lib.runDetails.*;
+
+/**
+ *
+ * @author Rushabh Modi
+ */
+public class IOManagerTest2 {
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String... args) throws IOException{
+          System.out.println("enter file name:-");
+          String s;
+          for(s=sc.nextLine();s=="";s=sc.nextLine());
+        IntIODetail io;
+        Path pi;
+        pi=Paths.get("Data",s).toAbsolutePath();
+        io=IOManager.getIODetail(pi);
+        pi=Paths.get("input2.txt");
+        Files.write(pi,io.getAllInput(),StandardOpenOption.CREATE);
+        pi=Paths.get("output2.txt");
+        Files.write(pi,io.getAllOutput(),StandardOpenOption.CREATE);
+    }
+}
