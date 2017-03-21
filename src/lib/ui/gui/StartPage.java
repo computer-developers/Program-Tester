504f504185959ca44ca047fb70a5f097d1a7be6f
Parth Doshi <doshiparth007@gmail.com>
2017-03-21 12:39:36 +0530

    Updating StartPage

--- src/lib/ui/gui/StartPage.java
+++ src/lib/ui/gui/StartPage.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 import java.io.*;
 import java.util.List;
 import javax.swing.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import lib.userModule.result.IntProgramState;
 
 /**
@@ -21,6 +22,7 @@ public class StartPage extends javax.swing.JPanel implements ActionListener {
      JLabel[] jl=new JLabel[10];
      Long[] spid=new Long[10];
      UserGUI ugi = null;
+     JButton close;
      private List<? extends IntProgramState> ps;
      public StartPage(List<? extends IntProgramState> ps,UserGUI ugi) {
         this.ps=ps;
@@ -31,10 +33,19 @@ public class StartPage extends javax.swing.JPanel implements ActionListener {
              jb[i]=new JButton("Attempt This");
              spid[i] = ips.getProgramID();
              jb[i].addActionListener(this);
+             add(jb[i]);
+             add(jl[i]);
              i++;
         }  
+        JButton close=new JButton("Close Program");
+        add(close);
+        close.addActionListener(new ActionListener() {
+             public void actionPerformed(ActionEvent e) {
+             ugi.close();
+           }
+        });
      }
-
+     
      @Override
      public void actionPerformed(ActionEvent e) {
           ugi.display(spid[e.getID()]);
