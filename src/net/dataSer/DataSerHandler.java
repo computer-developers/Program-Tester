e28334a3eab40df4264be3093a20e79251d1abc9
Neel Patel <neel.patel.2012.np@gmail.com>
2017-03-25 18:56:30 +0530

    add copywrite header & remove unwanted imports.

--- src/net/dataSer/DataSerHandler.java
+++ src/net/dataSer/DataSerHandler.java
@@ -1,18 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* Copyright 2017 Program Tester Team
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+
+*     http://www.apache.org/licenses/LICENSE-2.0
+
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* package lib.adminModule;
+*/
 package net.dataSer;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.rmi.RemoteException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import static programtester.config.Configuration.getDefaultDir;
 import static programtester.config.Configuration.getDefaultProDir;
 
--- src/net/flow/clientFlow/ClientFlow.java
+++ src/net/flow/clientFlow/ClientFlow.java
@@ -1,8 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* Copyright 2017 Program Tester Team
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+
+*     http://www.apache.org/licenses/LICENSE-2.0
+
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* package lib.adminModule;
+*/
 package net.flow.clientFlow;
 
 import java.rmi.Naming;
@@ -11,9 +22,6 @@ import java.rmi.registry.Registry;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.function.Consumer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import net.UrlTools;
 import net.flow.dataSerFlow.DataSerFlow;
 import net.logSer.IntRemoteLog;
 import net.mainSer.IntMainSer;
